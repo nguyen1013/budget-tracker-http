b8042fe161ada4c03a15f74832051b94bb450881
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-04-17 08:40:01 +0200

    minor update typo

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -2,23 +2,28 @@ import { useContext, useState } from "react";
 import { BudgetContext } from "../context/BudgetContext";
 import Confirm from "./Confirm";
 
-export default function Transaction({transaction}) {
-    const {deleteTransaction} = useContext(BudgetContext);
-    const [showConfirm, setShowConfirm] = useState(false);
+export default function Transaction({ transaction }) {
+  const { deleteTransaction } = useContext(BudgetContext);
+  const [showConfirm, setShowConfirm] = useState(false);
 
-    //console.log(transaction);
-    return (
-        <>
-            <li className={transaction.amount > 0 ? 'income' : 'expense'}>{transaction.description} <button onClick={() => setShowConfirm(true)}>X</button> </li>
+  //console.log(transaction);
+  return (
+    <>
+      <li className={transaction.amount > 0 ? "income" : "expense"}>
+        {transaction.description}{" "}
+        <button onClick={() => setShowConfirm(true)}>X</button>{" "}
+      </li>
 
-            {showConfirm && 
-                <Confirm message='Are you sure?' 
-                    onCancel={() => setShowConfirm(false)} 
-                    onConfirm={() => {
-                        deleteTransaction(transaction.id);
-                        setShowConfirm(false);
-                    }}
-                />}
-        </>
-    );
+      {showConfirm && (
+        <Confirm
+          message="Are you sure?"
+          onCancel={() => setShowConfirm(false)}
+          onConfirm={() => {
+            deleteTransaction(transaction.id);
+            setShowConfirm(false);
+          }}
+        />
+      )}
+    </>
+  );
 }
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -3,18 +3,18 @@ import { BudgetContext } from "../context/BudgetContext";
 import Transaction from "./Transaction";
 
 export default function TransactionList() {
-    const { transactions } = useContext(BudgetContext);
+  const { transactions } = useContext(BudgetContext);
 
-    return (
-        <div>
-            <h3>Transactions</h3>
-            {transactions && (
-                <ul className="transaction-list">
-                    {transactions.map(transaction => (
-                        <Transaction key={transaction.id} transaction={transaction}/>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <h3>Transactions</h3>
+      {transactions && (
+        <ul className="transaction-list">
+          {transactions.map((transaction) => (
+            <Transaction key={transaction.id} transaction={transaction} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
